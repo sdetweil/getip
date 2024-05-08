719ccbe0a6a005d4c0c25c4ee19ae9afcd6a8807
sam detweiler <sdetweil@gmail.com>
2024-05-08 09:21:08 -0500

    Update node_helper.js
    take out extraneous comment

--- node_helper.js
+++ node_helper.js
@@ -4,9 +4,8 @@ module.exports = NodeHelper.create({
 
 
 	start: function(){ 
-			console.log(this.name+" setup for ip address get")
+	    console.log(this.name+" setup for ip address get")
 	    this.expressApp.get("/modules/"+this.name, (req, res) => {
-	      // redirect to config form
 	      console.log(this.name+" responding with "+'{"address":"'+req.socket.remoteAddress+'"}')
 	      res.send('{"address":"'+req.socket.remoteAddress+'"}')
 	    });
